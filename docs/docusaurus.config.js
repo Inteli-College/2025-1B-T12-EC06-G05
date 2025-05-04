dcb079bac15b77f0b9caacee1ced09086413c595
MiguelClaret <miguel.claret@sou.inteli.edu.br>
2025-05-04 18:36:15 -0300

    fix: correção para o deploy

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Inteli-College", // Usually your GitHub org/user name.
-  projectName: "/2025-1B-T12-EC06-G05", // Usually your repo name.
+  projectName: "2025-1B-T12-EC06-G05", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
