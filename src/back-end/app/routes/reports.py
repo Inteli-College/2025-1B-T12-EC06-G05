145dab1384f5b3a33d114b5dc61e5337c9e0258d
MiguelClaret <miguel.claret@sou.inteli.edu.br>
2025-06-11 11:56:30 -0300

    feat: implementa try e catch para pegar o erros na rota de gerar relatorio

--- src/back-end/app/routes/reports.py
+++ src/back-end/app/routes/reports.py
@@ -5,14 +5,17 @@ from flask_jwt_extended import jwt_required
 report_bp = Blueprint('report', __name__, url_prefix='/report')
 
 @report_bp.route('/<int:id_predio>', methods=['GET'])
-#@jwt_required()
+@jwt_required()
 def teste(id_predio):
-    pdf_buffer = download_relatorio(id_predio)
+    pdf_buffer, status = download_relatorio(id_predio)
 
-    return send_file(
-        pdf_buffer,
-        as_attachment=True,
-        download_name=f"relatorio_{id_predio}.pdf",
-        mimetype="application/pdf"
-    )
+    if status == 200:
+        return send_file(
+            pdf_buffer,
+            as_attachment=True,
+            download_name=f"relatorio_id_predio{id_predio}.pdf",
+            mimetype="application/pdf"
+        ), status
+    
+    return pdf_buffer, status
 
--- src/back-end/app/services/report_services.py
+++ src/back-end/app/services/report_services.py
@@ -1,15 +1,10 @@
-from ..models.fissure import Fissure
-from ..models.building import Building
 from .building_services import  *
 from .expedition_services import *
 from .fissure_service import  *
-from ..models.image import Image
 from io import BytesIO
 import json
 from datetime import datetime
 from fpdf import FPDF
-from ...config.database import db
-from flask import jsonify
 from collections import defaultdict
 import requests
 import tempfile
@@ -18,85 +13,88 @@ from PIL import Image
 from io import BytesIO
 import imghdr
 
-
-
     
 def download_relatorio(id_predio):
-    resBuilding, _ = get_building_by_id(5)
-    building = resBuilding.get_json()["building"]
+    try:
+        resBuilding, statusResBuilding = get_building_by_id(id_predio)
+        if statusResBuilding == 500:
+            raise Exception("Não existe prédio para esse id!")
+        
+        building = resBuilding.get_json()["building"]
 
-    resExpedition, _ = get_expedition_by_id(building["id_expedicao"])
-    expedition = resExpedition.get_json()["expedition"]
+        resExpedition, statusResExpedition = get_expedition_by_id(building["id_expedicao"])
+        if statusResExpedition == 500:
+            raise Exception("Erro ao encontrar a expedição ligada a esse prédio")
+        
+        expedition = resExpedition.get_json()["expedition"]
 
-    resFissures, _ = get_fissures_by_predio(id_predio)
-    fissures = resFissures.get_json()["fissures"]
+        resFissures, statusResFissures = get_fissures_by_predio(id_predio)
+        if statusResFissures == 500:
+            raise Exception("Não há fissuras para esse prédio!")
+        fissures = resFissures.get_json()["fissures"]
 
-    total_fissures = len(fissures["termica"]) + len(fissures["retracao"])
-    
-    fissuras_termicas = fissures["termica"]
-    fissuras_retracao = fissures["retracao"]
+        fissuras_termicas = fissures["termica"]
+        fissuras_retracao = fissures["retracao"]
 
-    distribuicao_completa = defaultdict(lambda: {"termicas": 0, "retracao": 0, "total": 0})
+        total_fissures = len(fissuras_termicas) + len(fissuras_retracao)
+        
 
-    # Atualiza com fissuras térmicas
-    for fissura in fissuras_termicas:
-        ori = fissura["orientacao"].lower()
-        distribuicao_completa[ori]["termicas"] += 1
+        distribuicao_completa = defaultdict(lambda: {"termicas": 0, "retracao": 0, "total": 0})
 
-    # Atualiza com fissuras de retração
-    for fissura in fissuras_retracao:
-        ori = fissura["orientacao"].lower()
-        distribuicao_completa[ori]["retracao"] += 1
+        for fissura in fissuras_termicas:
+            ori = fissura["orientacao"].lower()
+            distribuicao_completa[ori]["termicas"] += 1
 
-    # Calcula os totais por orientação
-    for ori, valores in distribuicao_completa.items():
-        valores["total"] = valores["termicas"] + valores["retracao"]
+        for fissura in fissuras_retracao:
+            ori = fissura["orientacao"].lower()
+            distribuicao_completa[ori]["retracao"] += 1
 
-    distribuicao_orientacao_completa = dict(distribuicao_completa)
+        for ori, valores in distribuicao_completa.items():
+            valores["total"] = valores["termicas"] + valores["retracao"]
 
-    # Dicionário para organizar fissuras por orientação
-    fissuras_por_orientacao = defaultdict(list)
+        distribuicao_orientacao_completa = dict(distribuicao_completa)
 
-    for fissura in fissuras_termicas:
-        fissura_com_tipo = fissura.copy()
-        fissuras_por_orientacao[fissura["orientacao"]].append(fissura_com_tipo)
+        fissuras_por_orientacao = defaultdict(list)
 
-    for fissura in fissuras_retracao:
-        fissura_com_tipo = fissura.copy()
-        fissuras_por_orientacao[fissura["orientacao"]].append(fissura_com_tipo)
+        for fissura in fissuras_termicas:
+            fissura_com_tipo = fissura.copy()
+            fissuras_por_orientacao[fissura["orientacao"]].append(fissura_com_tipo)
 
-    fissuras_agrupadas = dict(fissuras_por_orientacao)
-    # Converte para dict padrão para exibição
+        for fissura in fissuras_retracao:
+            fissura_com_tipo = fissura.copy()
+            fissuras_por_orientacao[fissura["orientacao"]].append(fissura_com_tipo)
 
-    info_relatorio = {
-        "nome_expedicao": expedition['nome'],
-        "nome_predio": building['nome'],
-        "endereco": expedition['localizacao'],
-        "data_inspecao": expedition['ultima_att'],
-        "responsavel": {
-            "nome": expedition["nome_responsavel"],
-            "email": expedition["email_responsavel"]
-        },
-        "total_fissuras": total_fissures,
-        "fissuras_termicas": len(fissures["termica"]),
-        "fissuras_retracao": len(fissures["retracao"]),
-        "distribuicao_orientacao": distribuicao_orientacao_completa,
-        "fissuras": fissuras_agrupadas
-    }
+        fissuras_agrupadas = dict(fissuras_por_orientacao)
 
-    pdf_buffer = generate_report(info_relatorio)
-    return pdf_buffer
+        info_relatorio = {
+            "nome_expedicao": expedition['nome'],
+            "nome_predio": building['nome'],
+            "endereco": expedition['localizacao'],
+            "data_inspecao": expedition['ultima_att'],
+            "responsavel": {
+                "nome": expedition["nome_responsavel"],
+                "email": expedition["email_responsavel"]
+            },
+            "total_fissuras": total_fissures,
+            "fissuras_termicas": len(fissuras_retracao),
+            "fissuras_retracao": len(fissuras_termicas),
+            "distribuicao_orientacao": distribuicao_orientacao_completa,
+            "fissuras": fissuras_agrupadas
+        }
+
+        pdf_buffer = generate_report(info_relatorio)
+        return pdf_buffer, 200
+    except Exception as e:
+        return jsonify({"error": "Houve um erro ao gerar o relatório " + str(e)}), 500
 
 
 
 def generate_report(dados_json):
-    # Carrega os dados JSON
     if isinstance(dados_json, str):
         dados = json.loads(dados_json)
     else:
         dados = dados_json
     
-    # Cria o objeto PDF
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
@@ -165,7 +163,6 @@ def generate_report(dados_json):
             pdf.ln(2)
             pdf.cell(200, 20, txt=f"Fissura {fissura.get('id', 'Erro')}", ln=1)
         
-            # Adiciona a imagem se existir
 
             if 'url_fissura' in fissura:
                 try:
@@ -175,12 +172,10 @@ def generate_report(dados_json):
                     # Verifica o tipo da imagem
                     tipo_imagem = imghdr.what(None, h=response.content)
                     
-                    # Se for webp, converte para jpeg
-                    if tipo_imagem == 'webp':
-                        # Abre o conteúdo como imagem PIL
+                    if tipo_imagem not in ['jpeg', 'png', 'gif']:
                         img = Image.open(BytesIO(response.content))
                         
-                        # Salva em um arquivo temporário como JPEG
+                        # Salva em um arquivo temporário como a extensão correta
                         with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp_file:
                             img.convert('RGB').save(tmp_file, format='JPEG')
                             tmp_file_path = tmp_file.name
@@ -214,10 +209,10 @@ def generate_report(dados_json):
                         f"- Na auditoria houve alteração: {'sim' if fissura.get('alteracao_auditoria', False) else 'não'}")
             
             pdf.multi_cell(0, 10, txt=descricao)
-            pdf.ln(10)  # Espaço entre fissuras
+            pdf.ln(10)  
 
-    pdf_bytes = pdf.output(dest='S').encode('latin1')  # Gera o conteúdo do PDF
-    buffer = BytesIO(pdf_bytes)  # Coloca num BytesIO
+    pdf_bytes = pdf.output(dest='S').encode('latin1')  
+    buffer = BytesIO(pdf_bytes)
 
     buffer.seek(0)
     return buffer
