5e2b658154b248164427e542d0d520bed52fe703
yasmimpassos <yasmim.passos@sou.inteli.edu.br>
2025-06-14 18:41:33 -0300

    feat: ajusta a rota rodar modelo no streamlit

--- src/aplicacaoCampo/services/model.py
+++ src/aplicacaoCampo/services/model.py
@@ -18,7 +18,6 @@ def run_detection_model(model_path, image_folder):
 
     crack_counts = {}
     images_with_cracks = []
-    fissures_per_image = {}
 
     for img_path in images:
         img_name = os.path.basename(img_path)
@@ -29,18 +28,12 @@ def run_detection_model(model_path, image_folder):
 
         if len(results.boxes) > 0:
             images_with_cracks.append(img_name)
-            fissures_per_image[img_name] = []
 
             for box in results.boxes:
                 class_id = int(box.cls[0])
                 class_name = model.names[class_id]
-                confidence = float(box.conf[0])
 
                 crack_counts[class_name] = crack_counts.get(class_name, 0) + 1
-                fissures_per_image[img_name].append({
-                    "categoria": class_name,
-                    "confiabilidade": confidence
-                })
 
     crack_info_path = os.path.join(results_folder, "crack_info.json")
     with open(crack_info_path, 'w') as f:
@@ -48,10 +41,6 @@ def run_detection_model(model_path, image_folder):
             'images_with_cracks': images_with_cracks,
             'crack_counts': crack_counts
         }, f)
-
-    fissures_json_path = os.path.join(results_folder, "fissures_per_image.json")
-    with open(fissures_json_path, 'w') as f:
-        json.dump(fissures_per_image, f)
 
     return "Modelo executado com sucesso!", crack_counts
 
